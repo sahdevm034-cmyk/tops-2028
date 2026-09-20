956df72c7b5518565d479cb9af5cadeaff532d1c
sahdev-mali <sahdevm034@gmail.com>
2026-09-20 14:59:27 +0530

    assignments add

---
+++ software engineer/C language/Class/01_datatype.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main (){
+    printf ("hello user\n");
+    int i = 12;
+    float f = 12.5;
+    char c = 'a';
+    printf("1\n");
+    printf("i = %d\n", i);
+    printf("f = %f\n ",f);
+    printf("c =  %c\n",c);
+    
+
+}
